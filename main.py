75b4f9cd62c1b2391685f201500adb3a393a2d69
tanermaddocks <maddockstaner@gmail.com>
2024-11-16 09:27:25 +1000

    cli commands and seed students table
    format changes on some files, and fixed port issue

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 
 from flask import Flask
+
 from init import db, ma
+from controllers.cli_controller import db_commands
 
 def create_app():
     app = Flask(__name__)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+    
     db.init_app(app)
     ma.init_app(app)
+
+    app.register_blueprint(db_commands)
     return app
