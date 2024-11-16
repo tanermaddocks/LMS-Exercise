175d48893cb5cbb6559396b2f64122b3de245f41
tanermaddocks <maddockstaner@gmail.com>
2024-11-16 10:25:58 +1000

    format changes

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -5,30 +5,37 @@ from models.student import Student
 
 db_commands = Blueprint("db", __name__)
 
+
 @db_commands.cli.command("create")
 def create_tables():
     db.create_all()
     print("Tables created")
+
 
 @db_commands.cli.command("drop")
 def drop_tables():
     db.drop_all()
     print("Tables dropped")
 
+
 @db_commands.cli.command("seed")
 def seed_tables():
+
     students = [
         Student(
-            name="Ben Learner",
-            email="supernerd@email.com",
+            name="Student 1",
+            email="student1@email.com",
             address="Sydney"
         ),
         Student(
-            name="Will Study",
-            email="doingstuff@guy.com",
+            name="Student 2",
+            email="student2@email.com",
             address="Melbourne"
         )
     ]
+
     db.session.add_all(students)
+
     db.session.commit()
+
     print("Tables seeded")
