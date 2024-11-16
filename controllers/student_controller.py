39c9c5b7b10c58cd5bcf8cb6316269c0d691b6db
tanermaddocks <maddockstaner@gmail.com>
2024-11-16 11:16:40 +1000

    create and delete functions created for students controllers

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 
 from init import db
 from models.student import Student, students_schema, student_schema
@@ -30,4 +32,51 @@ def get_student(student_id):
         data = student_schema.dump(student)
         return data
     else:
+        return {"message": f"Student with id {student_id} does not exist"}, 404
+
+
+# Create - /students - POST
+@students_bp.route("/", methods=["POST"])
+def create_student():
+    try:
+        # get information from the request body
+        body_data = request.get_json()
+        # create student instance
+        new_student = Student(
+            name=body_data.get("name"),
+            email=body_data.get("email"),
+            address=body_data.get("address")
+        )
+        # add to the session
+        db.session.add(new_student)
+        # commit
+        db.session.commit()
+        # return a response
+        return student_schema.dump(new_student), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not null violation
+            return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 409
+        
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            # unique constraint violation
+            return {"message": "Email address already in use"}, 409
+
+
+# Delete - /students/id - DELETE
+@students_bp.route("/<int:student_id>", methods=["DELETE"])
+def delete_student(student_id):
+    # find the student to be deleted using id
+    stmt = db.select(Student).filter_by(id=student_id)
+    student = db.session.scalar(stmt)
+    # if student exists
+    if student:
+        # delete
+        db.session.delete(student)
+        db.session.commit()
+        # return some response
+        return {"message": f"Student '{student.name}' deleted successfully"}
+    # else
+    else:
+        # return some error response
         return {"message": f"Student with id {student_id} does not exist"}, 404
