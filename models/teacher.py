8cdce3b5f41c475bb222580da70261a613224cd1
tanermaddocks <maddockstaner@gmail.com>
2024-11-18 18:45:35 +1000

    added comment to line 8

---
+++ models/teacher.py
@@ -0,0 +1,16 @@
+from init import db, ma
+
+class Teacher(db.Model):
+    __tablename__ = "teachers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    department = db.Column(db.String(100), nullable=False, default="english") # may produce error
+    address = db.Column(db.String(100))
+
+class TeacherSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "department", "address")
+
+teacher_schema = TeacherSchema()
+teachers_schema = TeacherSchema(many=True)
