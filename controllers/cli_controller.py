3bb370dc77396dee2741b8be58ed3554669d0e29
tanermaddocks <maddockstaner@gmail.com>
2024-11-20 19:04:54 +1000

    added seed data for courses

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from init import db
 from models.student import Student
 from models.teacher import Teacher
+from models.courses import Course
 
 db_commands = Blueprint("db", __name__)
 
@@ -51,7 +52,32 @@ def seed_tables():
     ]
 
     db.session.add_all(teachers)
+    db.session.commit() # commit teachers and students
 
-    db.session.commit()
+    courses = [
+        Course(
+            name="Course 1",
+            duration=1,
+            teacher_id=teachers[0].id
+        ),
+        Course(
+            name="Course 2",
+            duration=0.5,
+            teacher_id=teachers[0].id
+        ),
+        Course(
+            name="Course 3",
+            duration=2,
+            teacher_id=teachers[1].id
+        ),
+        Course(
+            name="Course 4",
+            duration=0.5,
+            teacher_id=teachers[0].id
+        )
+    ]
+
+    db.session.add_all(courses)
+    db.session.commit() # commit courses
 
     print("Tables seeded")
