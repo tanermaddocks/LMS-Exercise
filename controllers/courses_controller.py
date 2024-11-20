322e99ddecef8dbdf9e08c0301009c52fddf21fa
tanermaddocks <maddockstaner@gmail.com>
2024-11-20 19:59:19 +1000

    Implement read operation on courses entity

--- controllers/courses_controller.py
+++ controllers/courses_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 from init import db
-from models.course import Course, courses_schema
+from models.course import Course, courses_schema, course_schema
 
 courses_bp = Blueprint("courses", __name__, url_prefix="/courses")
 
@@ -12,3 +12,13 @@ def get_courses():
     stmt = db.select(Course)
     courses_list = db.session.scalars(stmt)
     return courses_schema.dump(courses_list)
+
+# Read one
+@courses_bp.route("/<int:course_id>")
+def get_course(course_id):
+    stmt = db.select(Course).filter_by(id=course_id)
+    course = db.session.scalar(stmt)
+    if course:
+        return course_schema.dump(course)
+    else:
+        return {"message": f"Course with id {course_id} does not exist"}, 404
